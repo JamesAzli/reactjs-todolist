b1fdab544850f08b456b22dfd542ac81eea355a5
JamesAzli <azlijames03@gmail.com>
2025-02-28 13:55:08 +0800

    chore: made adding by pressing enter

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react';
 
 export default function TodoInput(props) {
-  const {handleAddTodos, todoValue, setTodoValue} = props
+  const { handleAddTodos, todoValue, setTodoValue } = props;
+
+  const addTodo = () => {
+    if (todoValue.trim() !== '') {
+      handleAddTodos(todoValue);
+      setTodoValue('');
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      addTodo(); // 👈 Call the same function on Enter key press
+    }
+  };
 
   return (
     <header>
-        <input value={todoValue} onChange={(e) => {
-          setTodoValue(e.target.value)
-        }}
-         placeholder='Enter To-Do' />
-        <button onClick={() =>{
-          handleAddTodos(todoValue)
-          setTodoValue('')
-        }}>Add</button>
+      <input
+        value={todoValue}
+        onChange={(e) => setTodoValue(e.target.value)}
+        onKeyDown={handleKeyDown} // 👈 Add todo on Enter
+        placeholder="Enter To-Do"
+      />
+      <button onClick={addTodo}>Add</button> {/* 👈 Add todo on button click */}
     </header>
-  )
+  );
 }
